6741d33fe6a884df5d0ef8dc44f30324166a74dd
hnhatcuong <hnhatcuong@HNHATCUONG>
2019-01-03 17:53:49 +0700

    done sidebar background and linear overlay

--- src/components/page/main/layout/SideBar.js
+++ src/components/page/main/layout/SideBar.js
@@ -1,11 +1,38 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
+const dataImage = require("../../../../static/img/sidebar-5.jpg");
 export default class SideBar extends Component {
   render() {
     return (
-      <div>
-        sideBar
+      <div className="sidebar" data-color="purple" data-image={dataImage}>
+        <div className="sidebar-wrapper">
+          <div className="logo">
+            <a href="#" className="simple-text">ADMINSITE</a>
+          </div>
+          <ul className="nav flex-column">
+            <li className="active">
+              <a href="#">
+              <i className="fa fa-address-card" aria-hidden="true"></i>
+                <p>Dashboard</p>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+              <i className="fa fa-address-card" aria-hidden="true"></i>
+                <p>Dashboard</p>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+              <i className="fa fa-address-card" aria-hidden="true"></i>
+                <p>Dashboard</p>
+              </a>
+            </li>
+          </ul>
+        </div>
+        {/* 
+Tip 1: you can change the color of the sidebar using: data-color="blue | azure | green | orange | red | purple"
+Tip 2: you can also add an image using data-image tag */}
       </div>
-    )
+    );
   }
 }
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
 import { Provider } from "react-redux";
-import "../styles/index.css";
+import "../styles/index.scss";
 import SideBar from "../components/page/main/layout/SideBar";
 class App extends Component {
   render() {
-    return <SideBar />;
+    return (
+      <div className="wrapper">
+        <SideBar />
+      </div>
+    );
   }
 }
 export default App;
