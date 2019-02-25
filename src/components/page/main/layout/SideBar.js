a0178d3c916686ffab3ae999fbc46ab69d4cb53e
hnhatcuong <hnhatcuong@HNHATCUONG>
2019-02-25 11:49:33 +0700

    change icon nav to svg  - [bug] bug in nav toggle

--- src/components/page/main/layout/SideBar.js
+++ src/components/page/main/layout/SideBar.js
@@ -3,16 +3,28 @@ const dataImage = require("../../../../static/img/sidebar-5.jpg");
 export default class SideBar extends Component {
   constructor(props) {
     super(props);
-    
   }
   render() {
     return (
-      <div className="sidebar" id="sidebar" data-color="pink" data-image={dataImage}>
+      <div
+        className="sidebar"
+        id="sidebar"
+        data-color="pink"
+        data-image={dataImage}
+      >
         <div className="sidebar-wrapper">
           <div className="logo">
             <div href="#" className="simple-text">
-            <i className="icon-logo-1-cuong" aria-hidden="true"></i>
-            <a href="#" className="logo-text">Dashboard</a>
+              {/* <i className="icon-logo-1-cuong" aria-hidden="true" /> */}
+              <svg
+                  src={require("../../../../static/icon/logo.svg")}
+                  focusable="false"
+                  className="svg nav-custom-icon "
+                  alt=""
+                />
+              <a href="#" className="logo-text">
+                Dashboard
+              </a>
             </div>
           </div>
           <ul className="nav flex-column">
@@ -24,11 +36,15 @@ export default class SideBar extends Component {
                 href="#dashboard"
                 aria-expanded="false"
                 aria-controls="dashboard"
-                
               >
-                <i class="icon-dashboard"  data-icon="a"></i>
-                
-                <p>Dashboard</p>
+                {/* <i class="icon-dashboard"  data-icon="a"></i> */}
+                <svg
+                  src={require("../../../../static/icon/dashboardSelected.svg")}
+                  focusable="false"
+                  className="svg nav-custom-icon"
+                  alt=""
+                />
+             Dashboard
               </a>
               <ul className="nav collapse" id="dashboard">
                 <li>
@@ -51,8 +67,14 @@ export default class SideBar extends Component {
                 aria-expanded="false"
                 aria-controls="layout"
               >
-               <i class="icon-layout" data-icon="b"></i>
-                <p>Layout</p>
+                {/* <i class="icon-layout" data-icon="b" /> */}
+                <svg
+                  src={require("../../../../static/icon/layout.svg")}
+                  focusable="false"
+                  className="svg nav-custom-icon"
+                  alt=""
+                />
+              Layout
               </a>
               <ul className="nav collapse" id="layout">
                 <li>
@@ -75,8 +97,14 @@ export default class SideBar extends Component {
                 aria-expanded="false"
                 aria-controls="ui-components"
               >
-              <i class="icon-ui-components" data-icon="c"></i>
-                <p>UI Components</p>
+                {/* <i class="icon-ui-components" data-icon="c" /> */}
+                <svg
+                  src={require("../../../../static/icon/uiComponents.svg")}
+                  focusable="false"
+                  className="svg nav-custom-icon"
+                  alt=""
+                />
+               UI Components
               </a>
               <ul className="nav collapse" id="ui-components">
                 <li>
@@ -99,8 +127,14 @@ export default class SideBar extends Component {
                 aria-expanded="false"
                 aria-controls="form"
               >
-           <i class="icon-forms" data-icon="e"></i>
-                <p>Form</p>
+                {/* <i class="icon-forms" data-icon="e" /> */}
+                <svg
+                  src={require("../../../../static/icon/forms.svg")}
+                  focusable="false"
+                  className="svg nav-custom-icon"
+                  alt=""
+                />
+              Form
               </a>
               <ul className="nav collapse" id="form">
                 <li>
@@ -123,8 +157,14 @@ export default class SideBar extends Component {
                 aria-expanded="false"
                 aria-controls="table"
               >
-              <i class="icon-table" data-icon="d"></i>
-                <p>Table</p>
+                {/* <i class="icon-table" data-icon="d" /> */}
+                <svg
+                  src={require("../../../../static/icon/table.svg")}
+                  focusable="false"
+                  className="svg nav-custom-icon"
+                  alt=""
+                />
+                Table  
               </a>
               <ul className="nav collapse" id="table">
                 <li>
